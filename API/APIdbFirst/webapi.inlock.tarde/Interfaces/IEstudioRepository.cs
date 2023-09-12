06f4f512634dfafb950b9b88d9795db0de502bc5
Vinicius Vieira <viniividrade@gmail.com>
2023-09-12 17:25:00 -0300

    API com entityframework

---
+++ API/APIdbFirst/webapi.inlock.tarde/Interfaces/IEstudioRepository.cs
@@ -0,0 +1,20 @@
+﻿using webapi.inlock.tarde.Domains;
+
+namespace webapi.inlock.tarde.Interfaces
+{
+    public interface IEstudioRepository
+    {
+        List<Estudio> Listar();
+
+        Estudio BuscaPorId(Guid id);
+
+        void Cadastrar(Estudio estudio);
+
+        void Atualizar(Guid id, Estudio estudio);
+
+        void Deletar(Guid id);
+
+        List<Estudio> ListarComJogos();
+
+    }
+}
---
+++ API/APIdbFirst/webapi.inlock.tarde/Repositories/EstudioRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using webapi.inlock.tarde.Contexts;
+using webapi.inlock.tarde.Domains;
+using webapi.inlock.tarde.Interfaces;
+
+namespace webapi.inlock.tarde.Repositories
+{
+    public class EstudioRepository : IEstudioRepository
+    {
+        InLockContext AcessaBanco = new InLockContext();
+
+        public void Atualizar(Guid id, Estudio estudio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudio BuscaPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Estudio estudio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudio> Listar()
+        {
+            return AcessaBanco.Estudios.ToList();
+        }
+
+        public List<Estudio> ListarComJogos()
+        {
+            return AcessaBanco.Estudios.Include(e => e.Jogos).ToList();
+        }
+    }
+}
