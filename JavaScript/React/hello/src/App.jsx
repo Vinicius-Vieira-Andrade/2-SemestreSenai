7994db349542876362c47e72ed49a7e6a810f6e3
Vinicius Vieira <viniividrade@gmail.com>
2023-10-31 17:05:47 -0300

    eventplus react em andamento

--- JavaScript/React/hello/src/App.jsx
+++ JavaScript/React/hello/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Rotas from "./routes";
+import Rotas from "../routes";
 
 function App() {
   return <Rotas />; //returnando as routes
--- JavaScript/React/hello/src/routes.js
+++ JavaScript/React/hello/src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import dos componentes de rota 'import BrowserRouter' adicionar o 'Router' e 'Route'
-import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ProdutoPage from "./pages/ProdutoPage/ProdutoPage";
@@ -11,9 +11,9 @@ const Rotas = () => {
     //criar a estrutura de rotas
     <BrowserRouter>
       <Routes>
-        <Route element={<HomePage />} path="/" exact />
-        <Route element={<LoginPage />} path="/login" />
-        <Route element={<ProdutoPage />} path="/produtos" />
+        <Route element={<HomePage />} path='/' exact /> 
+        <Route element={<LoginPage />} path='/login' /> {/*quando acessar no url o "/login" ele me renderiza a "LoginPage"*/}
+        <Route element={<ProdutoPage />} path='/produtos' />
       </Routes>
     </BrowserRouter>
   );
